d1dfb266c6962b5670d4b4d2f669e0ae160012ce
Natank25 <natank2511@gmail.com>
2024-05-02 20:42:35 +0200

    First commit

--- forge/src/main/java/io/github/natank25/scp_byo/forge/Scp_byoForge.java
+++ forge/src/main/java/io/github/natank25/scp_byo/forge/Scp_byoForge.java
@@ -2,16 +2,10 @@ package io.github.natank25.scp_byo.forge;
 
 import io.github.natank25.scp_byo.Scp_byo;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import dev.architectury.platform.forge.EventBuses;
 
-import static io.github.natank25.scp_byo.Scp_byo.MOD_ID;
-
-@Mod(MOD_ID)
+@Mod(Scp_byo.MOD_ID)
 public class Scp_byoForge {
     public Scp_byoForge() {
-        EventBuses.registerModEventBus(MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         Scp_byo.init();
-        
     }
 }
